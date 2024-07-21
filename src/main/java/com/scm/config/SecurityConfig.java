a8c713160db885504e8e3bd22e0885151a8c8886
Your Name <you@example.com>
2024-07-21 23:35:43 +0530

    Security Routes

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
 
@@ -28,6 +31,7 @@ public class SecurityConfig {
 
 
 
+//configuration of Authentication providers
   @Bean
   public DaoAuthenticationProvider authenticationProvider() {
 
@@ -38,7 +42,84 @@ public class SecurityConfig {
     daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
     return daoAuthenticationProvider;
+
 }
+
+ @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // configuration
+
+        // urls configure kiay hai ki koun se public rangenge aur koun se private
+        httpSecurity.authorizeHttpRequests(authorize -> {
+            //  authorize.requestMatchers("/home", "/register", "/services").permitAll();
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        });
+
+        // form default login
+        // if any changes related to  form login se
+       
+        httpSecurity.formLogin(formLogin -> {
+
+        //     //
+        //     formLogin.loginPage("/login");
+        //     formLogin.loginProcessingUrl("/authenticate");
+        //     formLogin.successForwardUrl("/user/profile");
+        //     // formLogin.failureForwardUrl("/login?error=true");
+        //     // formLogin.defaultSuccessUrl("/home");
+        //     formLogin.usernameParameter("email");
+        //     formLogin.passwordParameter("password");
+
+        //     // formLogin.failureHandler(new AuthenticationFailureHandler() {
+
+        //     // @Override
+        //     // public void onAuthenticationFailure(HttpServletRequest request,
+        //     // HttpServletResponse response,
+        //     // AuthenticationException exception) throws IOException, ServletException {
+        //     // // TODO Auto-generated method stub
+        //     // throw new UnsupportedOperationException("Unimplemented method
+        //     // 'onAuthenticationFailure'");
+        //     // }
+
+        //     // });
+
+        //     // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+        //     // @Override
+        //     // public void onAuthenticationSuccess(HttpServletRequest request,
+        //     // HttpServletResponse response,
+        //     // Authentication authentication) throws IOException, ServletException {
+        //     // // TODO Auto-generated method stub
+        //     // throw new UnsupportedOperationException("Unimplemented method
+        //     // 'onAuthenticationSuccess'");
+        //     // }
+
+        //     // });
+        //     formLogin.failureHandler(authFailtureHandler);
+
+        // });
+
+        // httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        // // oauth configurations
+
+        // httpSecurity.oauth2Login(oauth -> {
+        //     oauth.loginPage("/login");
+        //     oauth.successHandler(handler);
+        // });
+
+        // httpSecurity.logout(logoutForm -> {
+        //     logoutForm.logoutUrl("/do-logout");
+        //     logoutForm.logoutSuccessUrl("/login?logout=true");
+        // });
+
+        return httpSecurity.build();
+
+    }
+
+
+
+
 
   @Bean
     public PasswordEncoder passwordEncoder() {
